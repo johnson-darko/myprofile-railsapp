bcf80f9192874e117bb88827a76778024bbb1da5
johnson darko <johnsondarko365@gmail.com>
2022-08-14 15:16:44 +0200

    done

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,7 +1,6 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :correct_user, only: [:edit, :update, :destroy]
+
   # GET /friends or /friends.json
   def index
     @friends = Friend.all
